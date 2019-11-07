421a8bd1a54da0dc6fc7a99fb16a5bb0dd6268ed
egnica <46502133+egnica@users.noreply.github.com>
2019-11-06 23:31:58 -0600

    Add files via upload

---
+++ Lab4_WinForms/Lab4_WinForms/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Lab4_WinForms
+{
+    partial class Hello
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxDecription = new System.Windows.Forms.TextBox();
+            this.textBoxAvailability = new System.Windows.Forms.TextBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.textBoxEmp = new System.Windows.Forms.TextBox();
+            this.WriteButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to the Product Form";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(243, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please enter the product information below";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(30, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Product Name:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(30, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(250, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product Model Number:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(30, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(250, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Product Decription:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Location = new System.Drawing.Point(30, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(250, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Product Availability:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label7.Location = new System.Drawing.Point(30, 350);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(250, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Product Stock Date:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label8.Location = new System.Drawing.Point(30, 400);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(250, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Product Emp. Check (Name):";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(300, 150);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(350, 28);
+            this.textBoxName.TabIndex = 8;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumber.Location = new System.Drawing.Point(300, 200);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(350, 28);
+            this.textBoxNumber.TabIndex = 9;
+            // 
+            // textBoxDecription
+            // 
+            this.textBoxDecription.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDecription.Location = new System.Drawing.Point(300, 250);
+            this.textBoxDecription.Name = "textBoxDecription";
+            this.textBoxDecription.Size = new System.Drawing.Size(350, 28);
+            this.textBoxDecription.TabIndex = 10;
+            // 
+            // textBoxAvailability
+            // 
+            this.textBoxAvailability.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAvailability.Location = new System.Drawing.Point(300, 300);
+            this.textBoxAvailability.Name = "textBoxAvailability";
+            this.textBoxAvailability.Size = new System.Drawing.Size(350, 28);
+            this.textBoxAvailability.TabIndex = 11;
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDate.Location = new System.Drawing.Point(300, 350);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(350, 28);
+            this.textBoxDate.TabIndex = 12;
+            // 
+            // textBoxEmp
+            // 
+            this.textBoxEmp.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmp.Location = new System.Drawing.Point(300, 398);
+            this.textBoxEmp.Name = "textBoxEmp";
+            this.textBoxEmp.Size = new System.Drawing.Size(350, 28);
+            this.textBoxEmp.TabIndex = 13;
+            // 
+            // WriteButton
+            // 
+            this.WriteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WriteButton.Location = new System.Drawing.Point(300, 461);
+            this.WriteButton.Name = "WriteButton";
+            this.WriteButton.Size = new System.Drawing.Size(135, 50);
+            this.WriteButton.TabIndex = 14;
+            this.WriteButton.Text = "Write";
+            this.WriteButton.UseVisualStyleBackColor = true;
+            this.WriteButton.Click += new System.EventHandler(this.WriteButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(515, 461);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(135, 50);
+            this.ClearButton.TabIndex = 15;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label9.Location = new System.Drawing.Point(310, 527);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(340, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Entry will be written to C:\\Users\\egnnica\\products.csv";
+            // 
+            // Hello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 587);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.WriteButton);
+            this.Controls.Add(this.textBoxEmp);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.textBoxAvailability);
+            this.Controls.Add(this.textBoxDecription);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Hello";
+            this.Text = "Product Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxDecription;
+        private System.Windows.Forms.TextBox textBoxAvailability;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.TextBox textBoxEmp;
+        private System.Windows.Forms.Button WriteButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Lab4_WinForms/Lab4_WinForms/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_WinForms
+{
+    public partial class Hello : Form
+    {
+        public Hello()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WriteButton_Click(object sender, EventArgs e)
+        {
+            string delim = ",";
+
+            if(textBoxName.Text != "" && textBoxNumber.Text != "" && textBoxEmp.Text != "" &&
+                textBoxAvailability.Text != "" && textBoxDate.Text != "" && textBoxDecription.Text != "")
+            {
+                string filePath = "C:\\Users\\egnnica\\products.csv";
+                FileStream outFile = new FileStream(filePath, FileMode.Append, FileAccess.Write);
+                StreamWriter writerCSV = new StreamWriter(outFile);
+
+                writerCSV.WriteLine(textBoxName.Text + delim + textBoxNumber.Text + delim + textBoxDecription.Text + delim + 
+                    textBoxAvailability.Text + delim + textBoxDate.Text + delim + textBoxEmp.Text);
+
+                writerCSV.Close();
+                outFile.Close();
+
+                textBoxName.Text = "";
+                textBoxNumber.Text = "";
+                textBoxEmp.Text = "";
+                textBoxAvailability.Text = "";
+                textBoxDate.Text = "";
+                textBoxDecription.Text = "";
+
+                textBoxName.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Must fiil in all boxes", "Fill boxes");
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            textBoxName.Text = "";
+            textBoxNumber.Text = "";
+            textBoxEmp.Text = "";
+            textBoxAvailability.Text = "";
+            textBoxDate.Text = "";
+            textBoxDecription.Text = "";
+
+            textBoxName.Focus();
+        }
+    }
+}
